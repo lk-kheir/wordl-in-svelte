6cb992b2faff95c3f258e6420f4ced20c5bed7da
louzanizineddine <louzanizineddine@gmail.com>
2022-07-20 21:38:48 +0100

    project setup

---
+++ wordle/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Board from "./components/Board.svelte";
+    import KeyBoard from "./components/KeyBoard.svelte";
+</script>
+<main>
+	<nav>Worde Clone</nav>
+
+    <Board/>
+    <KeyBoard/>
+</main>
+
+<style>
+    nav {
+        background-color: var(--bg);
+        color: white;
+        font-weight: bold;
+        padding: 15px;
+        font-size: 2em;
+        text-align: center;
+    }
+</style>
